f6439a8af612912105383bdf5829b816677af7a2
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2020-03-17 17:25:43 +0000

    Implement Exception handling within crypto functions

--- server/src/main/java/sec/dpas/Server.java
+++ server/src/main/java/sec/dpas/Server.java
@@ -14,13 +14,23 @@ import java.security.Key;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidKeyException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
 import javax.crypto.Cipher;
-import java.security.MessageDigest;
+import javax.crypto.NoSuchPaddingException; 
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.BadPaddingException;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
+
+
 /**
  * TODO!
  *
@@ -61,9 +71,7 @@ public class Server implements ClientAPI{
 
 
 
-    //TODO implement exceptions, otherwise wont compile
-    /*
-    public static Key readPrivateKey(String keypath) {
+    public static Key readPrivateKey(String keypath) throws FileNotFoundException, IOException {
         System.out.println("Reading key from file " + keypath + " ...");
         FileInputStream fis = new FileInputStream(keypath);
         byte[] encoded = new byte[fis.available()];
@@ -71,13 +79,20 @@ public class Server implements ClientAPI{
         fis.close();
 
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(encoded);
-        KeyFactory keyFac = KeyFactory.getInstance("RSA");
-        PrivateKey pub = keyFac.generatePrivate(spec);
-
-        return pub;
+	
+	KeyFactory keyFac;
+	PrivateKey priv = null;
+	try {
+		keyFac = KeyFactory.getInstance("RSA");
+        	priv = keyFac.generatePrivate(spec);
+	} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+		System.err.println("Something really weird happened while reading the private key");
+		System.exit(1);
+	}
+        return priv;
     }
 
-    public static Key readPublicKey(String keypath) {
+    public static Key readPublicKey(String keypath) throws FileNotFoundException, IOException {
         System.out.println("Reading key from file " + keypath + " ...");
         FileInputStream fis = new FileInputStream(keypath);
         byte[] encoded = new byte[fis.available()];
@@ -85,33 +100,75 @@ public class Server implements ClientAPI{
         fis.close();
 
         X509EncodedKeySpec spec = new X509EncodedKeySpec(encoded);
-        KeyFactory keyFac = KeyFactory.getInstance("RSA");
-        PublicKey pub = keyFac.generatePublic(spec);
-
+	KeyFactory keyFac;
+	PublicKey pub = null;
+	try {
+		keyFac = KeyFactory.getInstance("RSA");
+        	pub = keyFac.generatePublic(spec);
+	} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+		System.err.println("Something really weird happened while reading the public key");
+		System.exit(1);
+	}
         return pub;
     }
 
     public static byte[] sign(Key key, byte[] message) {
 
-	MessageDigest md = MessageDigest.getInstance("SHA-256");
+	MessageDigest md = null;
+	Cipher cipher = null;
+	try {
+		md = MessageDigest.getInstance("SHA-256");
+		cipher = Cipher.getInstance("RSA");
+	} catch (NoSuchAlgorithmException e) {
+		System.err.println("Thanos snapped RSA or SHA-256 out of existance");
+		System.exit(1);
+	} catch (NoSuchPaddingException e) {
+		System.err.println("No padding was provided so God must be messing with the Universe");
+		System.exit(1);
+	}
+
 	md.update(message);
 	byte[] digest = md.digest();
+	byte[] signature = null;
 
-	Cipher cipher = Cipher.getInstance("RSA");
-	cipher.init(Cipher.ENCRYPT_MODE, key);
-	return cipher.doFinal(digest);
+
+	try {	
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		signature = cipher.doFinal(digest);
+	} catch (BadPaddingException | InvalidKeyException | IllegalBlockSizeException e) {
+		System.err.println(e.getMessage());
+		System.exit(1);
+	}
+
+	return signature;
 
     }
 
     public static boolean verifySignature(Key key, byte[] message, byte[] signature) {
 
-	MessageDigest md = MessageDigest.getInstance("SHA-256");
-	md.update(message);
-	byte[] digest = md.digest();
+	MessageDigest md = null;
+	Cipher cipher = null;
+	try {
+		md = MessageDigest.getInstance("SHA-256");
+		cipher = Cipher.getInstance("RSA");
+	} catch (NoSuchAlgorithmException e) {
+		System.err.println("Thanos snapped RSA or SHA-256 out of existance");
+		System.exit(1);
+	} catch (NoSuchPaddingException e) {
+		System.err.println("No padding was provided so God must be messing with the Universe");
+		System.exit(1);
+	}
 
-	Cipher cipher = Cipher.getInstance("RSA");
-	cipher.init(Cipher.DECRYPT_MODE, key);
-	return Arrays.equals(digest, cipher.doFinal(digest));
-
-}*/
+	byte[] digest = md.digest(message);
+	boolean res = false;
+	try {
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		res = Arrays.equals(digest, cipher.doFinal(signature));
+	} catch (BadPaddingException | InvalidKeyException | IllegalBlockSizeException e) {
+		System.err.println(e.getMessage());
+		System.exit(1);
+	}
+	
+	return res;
+    }
 }
